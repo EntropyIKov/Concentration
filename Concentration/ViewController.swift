563d5786bbc5b4e9bc04b4cf3b1fce12d5315ece
Kovalenko Ilia <entropy@macbook-pro-kovalenko.local>
2018-05-09 00:57:10 +0300

    Lecture 4

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -18,13 +18,26 @@ class ViewController: UIViewController
     
     private(set) var flipCount = 0 {
         didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
+            updateFlipCountLabel();
         }
+    }
+    
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth : 5.0,
+            .strokeColor : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),
+            ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
     }
     
     @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBOutlet private weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
     
     @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
@@ -52,14 +65,17 @@ class ViewController: UIViewController
         }
     }
     
-    private var emojiChoices = ["😀", "😇", "😎", "😱", "😈", "👻", "😹", "👮🏿‍♀️", "🙅‍♂️"]
-    private var emoji = [Int: String]()
+//    private var emojiChoices = ["😀", "😇", "😎", "😱", "😈", "👻", "😹", "👮🏿‍♀️", "🙅‍♂️"]
+    private var emojiChoices = "😀😇😎😱😈👻😹👮🏿‍♀️🙅‍♂️"
+    
+    private var emoji = [Card:String]()
     
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card ] ?? "?"
     }
 }
 
